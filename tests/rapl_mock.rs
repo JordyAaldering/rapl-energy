a4d4289cfb53bb29b0c5fcc898628730f51c42cb
Jordy Aaldering <jordy.aaldering@ru.nl>
2024-06-21 10:46:40 +0200

    simple mock test

---
+++ tests/rapl_mock.rs
@@ -0,0 +1,29 @@
+use std::time::Instant;
+
+use rapl_energy as rapl;
+
+pub struct RaplMock {
+    handle: Instant,
+}
+
+impl rapl::RaplReader for RaplMock {
+    fn now(package_id: u8) -> Option<Self> {
+        if package_id < 4 {
+            let handle = Instant::now();
+            Some(RaplMock { handle })
+        } else {
+            None
+        }
+    }
+
+    fn elapsed(&self) -> u64 {
+        self.handle.elapsed().as_micros() as u64
+    }
+}
+
+#[test]
+fn dependency_injection() {
+    let packages = rapl::packages::<RaplMock>();
+    let elapsed: Vec<u64> = rapl::elapsed(packages).collect();
+    assert_eq!(4, elapsed.len());
+}
