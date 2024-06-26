5eed8079667998b387e2f8c1e98ef0df5b51b101
Jordy Aaldering <aalderingjordy@gmail.com>
2024-06-26 11:21:33 +0200

    rapl amd bindings

--- rapl-energy-c/examples/intel.c
+++ rapl-energy-c/examples/intel.c
@@ -2,7 +2,7 @@
 
 extern const int rapl_intel_start(void **rapl_ptr);
 extern const int rapl_intel_stop(void *rapl_ptr, void **elapsed_ptr);
-extern const int rapl_intel_free(void *rapl_ptr, void *elapsed_ptr);
+extern const void rapl_intel_free(void *rapl_ptr, void *elapsed_ptr);
 extern const int rapl_print(void *elapsed_ptr);
 
 // gcc -o intelc rapl-energy-c/examples/intel.c  target/debug/librapl_energy_c.a
@@ -17,6 +17,7 @@ int main()
     rapl_intel_stop(rapl_ptr, &elapsed_ptr);
     rapl_print(elapsed_ptr);
 
+    // TODO: This segfaults
     rapl_intel_free(rapl_ptr, elapsed_ptr);
     rapl_ptr = NULL;
     elapsed_ptr = NULL;
--- rapl-energy-c/libc.rs
+++ rapl-energy-c/libc.rs
@@ -3,13 +3,12 @@ use rapl_energy::*;
 const RAPL_NULLPTR: u32 = 1;
 const RAPL_SUCCESS: u32 = 0;
 
-#[no_mangle]
-pub extern "C" fn rapl_intel_start(rapl_ptr: *mut *mut RaplVec<RaplIntel>) -> u32 {
+fn rapl_start<T: RaplReader>(rapl_ptr: *mut *mut RaplVec<T>) -> u32 {
     unsafe {
         *rapl_ptr = std::ptr::null_mut();
     }
 
-    let packages = get_packages::<RaplIntel>();
+    let packages = get_packages::<T>();
 
     unsafe {
         *rapl_ptr = Box::into_raw(Box::new(packages));
@@ -19,7 +18,16 @@ pub extern "C" fn rapl_intel_start(rapl_ptr: *mut *mut RaplVec<RaplIntel>) -> u3
 }
 
 #[no_mangle]
-pub extern "C" fn rapl_intel_stop(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr: *mut *mut RaplVec<u64>) -> u32 {
+pub extern "C" fn rapl_intel_start(rapl_ptr: *mut *mut RaplVec<RaplIntel>) -> u32 {
+    rapl_start::<RaplIntel>(rapl_ptr)
+}
+
+#[no_mangle]
+pub extern "C" fn rapl_amd_start(rapl_ptr: *mut *mut RaplVec<RaplAMD>) -> u32 {
+    rapl_start::<RaplAMD>(rapl_ptr)
+}
+
+fn rapl_stop<T: RaplReader>(rapl_ptr: *mut RaplVec<T>, elapsed_ptr: *mut *mut RaplVec<u64>) -> u32 {
     if rapl_ptr.is_null() {
         return RAPL_NULLPTR;
     }
@@ -28,7 +36,7 @@ pub extern "C" fn rapl_intel_stop(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr
         Box::from_raw(rapl_ptr)
     };
 
-    let elapsed = get_elapsed(&rapl);
+    let elapsed = get_elapsed::<T>(&rapl);
 
     unsafe {
         *elapsed_ptr = Box::into_raw(Box::new(elapsed));
@@ -38,17 +46,37 @@ pub extern "C" fn rapl_intel_stop(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr
 }
 
 #[no_mangle]
-pub extern "C" fn rapl_intel_free(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr: *mut RaplVec<u64>) -> u32 {
-    if rapl_ptr.is_null() || elapsed_ptr.is_null() {
-        return RAPL_NULLPTR;
+pub extern "C" fn rapl_intel_stop(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr: *mut *mut RaplVec<u64>) -> u32 {
+    rapl_stop::<RaplIntel>(rapl_ptr, elapsed_ptr)
+}
+
+#[no_mangle]
+pub extern "C" fn rapl_amd_stop(rapl_ptr: *mut RaplVec<RaplAMD>, elapsed_ptr: *mut *mut RaplVec<u64>) -> u32 {
+    rapl_stop::<RaplAMD>(rapl_ptr, elapsed_ptr)
+}
+
+fn rapl_free<T: RaplReader>(rapl_ptr: *mut RaplVec<T>, elapsed_ptr: *mut RaplVec<u64>) {
+    if !rapl_ptr.is_null() {
+        unsafe {
+            drop(Box::from_raw(rapl_ptr));
+        }
     }
 
-    unsafe {
-        drop(Box::from_raw(rapl_ptr));
-        drop(Box::from_raw(elapsed_ptr));
+    if !elapsed_ptr.is_null() {
+        unsafe {
+            drop(Box::from_raw(elapsed_ptr));
+        }
     }
+}
 
-    RAPL_SUCCESS
+#[no_mangle]
+pub extern "C" fn rapl_intel_free(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr: *mut RaplVec<u64>) {
+    rapl_free::<RaplIntel>(rapl_ptr, elapsed_ptr)
+}
+
+#[no_mangle]
+pub extern "C" fn rapl_amd_free(rapl_ptr: *mut RaplVec<RaplAMD>, elapsed_ptr: *mut RaplVec<u64>) {
+    rapl_free::<RaplAMD>(rapl_ptr, elapsed_ptr)
 }
 
 #[no_mangle]
