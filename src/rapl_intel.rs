e7b32ee46db62e8230b35bf1dbb54f5206131830
Jordy Aaldering <aalderingjordy@gmail.com>
2024-06-26 08:44:57 +0200

    rapl intel prepare for getting subzones

--- src/rapl_intel.rs
+++ src/rapl_intel.rs
@@ -8,6 +8,7 @@ use crate::RaplReader;
 pub struct RaplIntel {
     handle: Mutex<File>,
     package_id: usize,
+    subzone_id: Option<usize>,
     energy_uj: u64,
 }
 
@@ -17,7 +18,7 @@ impl RaplReader for RaplIntel {
         let mut file = OpenOptions::new().read(true).open(&path).ok()?;
         let energy_uj = read_raw(&mut file);
         let handle = Mutex::new(file);
-        Some(RaplIntel { handle, package_id, energy_uj })
+        Some(RaplIntel { handle, package_id, subzone_id: None, energy_uj })
     }
 
     fn elapsed(&self) -> u64 {
@@ -27,7 +28,25 @@ impl RaplReader for RaplIntel {
     }
 
     fn label(&self) -> String {
-        format!("intel-rapl:{}", self.package_id)
+        if let Some(subzone_id) = self.subzone_id {
+            format!("intel-rapl:{}:{}", self.package_id, subzone_id)
+        } else {
+            format!("intel-rapl:{}", self.package_id)
+        }
+    }
+}
+
+impl RaplIntel {
+    /// Currently unused, but we might want this in the future.
+    /// If RaplAMD has a similar field this is easy, otherwise we need to find
+    /// a nice generic solution
+    #[allow(unused)]
+    fn subzone_now(package_id: usize, subzone_id: usize) -> Option<Self> {
+        let path = format!("/sys/class/powercap/intel-rapl:{}:{}/energy_uj", package_id, subzone_id);
+        let mut file = OpenOptions::new().read(true).open(&path).ok()?;
+        let energy_uj = read_raw(&mut file);
+        let handle = Mutex::new(file);
+        Some(RaplIntel { handle, package_id, subzone_id: Some(subzone_id), energy_uj })
     }
 }
 
