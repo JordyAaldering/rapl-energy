7a7ba25b4b68cab9dce555cb4893738a34b565a2
Jordy Aaldering <aalderingjordy@gmail.com>
2024-06-26 13:24:46 +0200

    makefile to install c library

---
+++ rapl-energy-c/rapl_energy_c.h
@@ -0,0 +1,23 @@
+#ifndef _RAPL_ENERGY_H_
+#define _RAPL_ENERGY_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const int rapl_amd_start(void **rapl_ptr);
+extern const int rapl_intel_start(void **rapl_ptr);
+
+extern const int rapl_amd_stop(void *rapl_ptr, void **elapsed_ptr);
+extern const int rapl_intel_stop(void *rapl_ptr, void **elapsed_ptr);
+
+extern const void rapl_amd_free(void *rapl_ptr, void *elapsed_ptr);
+extern const void rapl_intel_free(void *rapl_ptr, void *elapsed_ptr);
+
+extern const int rapl_print(void *elapsed_ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RAPL_ENERGY_H_ */
