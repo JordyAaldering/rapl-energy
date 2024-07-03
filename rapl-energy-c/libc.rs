a0f9df6dad2fef90ae4fa584d988928ff2b64782
Jordy Aaldering <aalderingjordy@gmail.com>
2024-07-03 14:48:41 +0200

    fix c bindings

--- rapl-energy-c/libc.rs
+++ rapl-energy-c/libc.rs
@@ -1,91 +1,38 @@
-use rapl_energy::*;
+use rapl_energy::Energy;
 
-const RAPL_NULLPTR: u32 = 1;
-const RAPL_SUCCESS: u32 = 0;
-
-fn rapl_start<T: RaplReader>(rapl_ptr: *mut *mut RaplVec<T>) -> u32 {
+#[no_mangle]
+pub extern "C" fn start_msr(msr_out: *mut *mut Energy) {
     unsafe {
-        *rapl_ptr = std::ptr::null_mut();
+        *msr_out = Box::into_raw(Box::new(Energy::msr()));
     }
+}
 
-    let packages = get_packages::<T>();
-
+#[no_mangle]
+pub extern "C" fn start_rapl(rapl_out: *mut *mut Energy) {
     unsafe {
-        *rapl_ptr = Box::into_raw(Box::new(packages));
-    }
-
-    RAPL_SUCCESS
-}
-
-#[no_mangle]
-pub extern "C" fn rapl_intel_start(rapl_ptr: *mut *mut RaplVec<RaplIntel>) -> u32 {
-    rapl_start::<RaplIntel>(rapl_ptr)
-}
-
-#[no_mangle]
-pub extern "C" fn rapl_amd_start(rapl_ptr: *mut *mut RaplVec<Msr>) -> u32 {
-    rapl_start::<Msr>(rapl_ptr)
-}
-
-fn rapl_stop<T: RaplReader>(rapl_ptr: *mut RaplVec<T>, elapsed_ptr: *mut *mut RaplVec<u64>) -> u32 {
-    if rapl_ptr.is_null() {
-        return RAPL_NULLPTR;
-    }
-
-    let rapl = unsafe {
-        Box::from_raw(rapl_ptr)
-    };
-
-    let elapsed = get_elapsed::<T>(&rapl);
-
-    unsafe {
-        *elapsed_ptr = Box::into_raw(Box::new(elapsed));
-    }
-
-    RAPL_SUCCESS
-}
-
-#[no_mangle]
-pub extern "C" fn rapl_intel_stop(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr: *mut *mut RaplVec<u64>) -> u32 {
-    rapl_stop::<RaplIntel>(rapl_ptr, elapsed_ptr)
-}
-
-#[no_mangle]
-pub extern "C" fn rapl_amd_stop(rapl_ptr: *mut RaplVec<Msr>, elapsed_ptr: *mut *mut RaplVec<u64>) -> u32 {
-    rapl_stop::<Msr>(rapl_ptr, elapsed_ptr)
-}
-
-fn rapl_free<T: RaplReader>(rapl_ptr: *mut RaplVec<T>, elapsed_ptr: *mut RaplVec<u64>) {
-    if !rapl_ptr.is_null() {
-        drop(unsafe { Box::from_raw(rapl_ptr) });
-    }
-
-    if !elapsed_ptr.is_null() {
-        drop(unsafe { Box::from_raw(elapsed_ptr) });
+        *rapl_out = Box::into_raw(Box::new(Energy::rapl()));
     }
 }
 
 #[no_mangle]
-pub extern "C" fn rapl_intel_free(rapl_ptr: *mut RaplVec<RaplIntel>, elapsed_ptr: *mut RaplVec<u64>) {
-    rapl_free::<RaplIntel>(rapl_ptr, elapsed_ptr)
-}
-
-#[no_mangle]
-pub extern "C" fn rapl_amd_free(rapl_ptr: *mut RaplVec<Msr>, elapsed_ptr: *mut RaplVec<u64>) {
-    rapl_free::<Msr>(rapl_ptr, elapsed_ptr)
-}
-
-#[no_mangle]
-pub extern "C" fn rapl_print(elapsed_ptr: *mut RaplVec<u64>) -> u32 {
-    if elapsed_ptr.is_null() {
-        return RAPL_NULLPTR;
+pub extern "C" fn print_energy(energy_in: *mut Energy) {
+    if energy_in.is_null() {
+        println!("nullptr");
+        return;
     }
 
-    let elapsed = unsafe {
-        Box::from_raw(elapsed_ptr)
-    };
+    let energy = unsafe { Box::from_raw(energy_in) };
+    let elapsed = energy.elapsed();
+    println!("{}", serde_json::to_string_pretty(&elapsed).unwrap());
+}
 
-    println!("{:?}", elapsed);
+#[no_mangle]
+pub extern "C" fn free_energy(energy_in: *mut Energy) {
+    if energy_in.is_null() {
+        println!("nullptr");
+        return;
+    }
 
-    RAPL_SUCCESS
+    let energy = unsafe { Box::from_raw(energy_in) };
+    drop(energy);
 }
--- rapl-energy-c/rapl_energy_c.h
+++ rapl-energy-c/rapl_energy_c.h
@@ -5,16 +5,10 @@
 extern "C" {
 #endif
 
-extern const int rapl_amd_start(void **rapl_ptr);
-extern const int rapl_intel_start(void **rapl_ptr);
-
-extern const int rapl_amd_stop(void *rapl_ptr, void **elapsed_ptr);
-extern const int rapl_intel_stop(void *rapl_ptr, void **elapsed_ptr);
-
-extern void rapl_amd_free(void *rapl_ptr, void *elapsed_ptr);
-extern void rapl_intel_free(void *rapl_ptr, void *elapsed_ptr);
-
-extern const int rapl_print(void *elapsed_ptr);
+extern void *start_msr(void **msr_out);
+extern void *start_rapl(void **rapl_out);
+extern void print_energy(void *energy_in);
+extern void free_energy(void *energy_in);
 
 #ifdef __cplusplus
 }
