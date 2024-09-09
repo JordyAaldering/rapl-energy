dc143acfd83d27c554d43b04c0b288e3f593c059
Jordy Aaldering <aalderingjordy@gmail.com>
2024-09-09 19:43:53 +0200

    add nvml support

--- src/nvml.rs
+++ src/nvml.rs
@@ -1,39 +1,66 @@
 use indexmap::IndexMap;
+use once_cell::sync::Lazy;
 
 use crate::Energy;
 
-pub struct Nvml {
-    nvml: nvml_wrapper::Nvml,
-    devices: IndexMap<u32, Result<u64, nvml_wrapper::error::NvmlError>>
+static NVML: Lazy<Option<nvml_wrapper::Nvml>> = Lazy::new(|| nvml_wrapper::Nvml::init().ok());
+
+pub struct Nvml<'a> {
+    devices: Vec<NvmlDevice<'a>>,
 }
 
-impl Nvml {
+pub struct NvmlDevice<'a> {
+    device: nvml_wrapper::Device<'a>,
+    name: String,
+    energy: u64,
+}
+
+impl<'a> Nvml<'a> {
     pub fn now() -> Option<Box<dyn Energy>> {
-        let nvml = nvml_wrapper::Nvml::init().ok()?;
-        let device_count = nvml.device_count().ok()?;
-        let devices = (0..device_count).map(|i| (i, read_nvml(&nvml, i))).collect();
-        Some(Box::new(Nvml { nvml, devices }))
+        let nvml = NVML.as_ref()?;
+        let count = nvml.device_count().ok()?;
+        let devices = (0..count).filter_map(NvmlDevice::new).collect();
+        Some(Box::new(Nvml { devices }))
     }
 }
 
-impl Energy for Nvml {
+impl<'a> Energy for Nvml<'a> {
     fn elapsed(&self) -> IndexMap<String, f64> {
-        self.devices.iter().map(|(&i, prev_energy)| {
-            if let Ok(prev_energy) = prev_energy {
-                let next_energy = read_nvml(&self.nvml, i).unwrap();
-                (i.to_string(), (next_energy - prev_energy) as f64)
-            } else {
-                (i.to_string(), f64::NAN)
-            }
+        self.devices.iter().map(|device| {
+            let name = device.name.clone();
+            let energy = device.elapsed();
+            (name, energy)
         }).collect()
     }
 
     fn elapsed_mut(&mut self) -> IndexMap<String, f64> {
-        todo!()
+        self.devices.iter_mut().map(|device| {
+            let name = device.name.clone();
+            let energy = device.elapsed_mut();
+            (name, energy)
+        }).collect()
     }
 }
 
-fn read_nvml(nvml: &nvml_wrapper::Nvml, index: u32) -> Result<u64, nvml_wrapper::error::NvmlError> {
-    let device = nvml.device_by_index(index)?;
-    device.total_energy_consumption()
+impl<'a> NvmlDevice<'a> {
+    fn new(index: u32) -> Option<NvmlDevice<'a>> {
+        let nvml = NVML.as_ref()?;
+        let device = nvml.device_by_index(index).ok()?;
+        let name = format!("GPU({}) {}", index, device.name().ok()?);
+        let energy = device.total_energy_consumption().ok()?;
+        Some(NvmlDevice { device, name, energy })
+    }
+
+    fn elapsed(&self) -> f64 {
+        let prev_energy = self.energy;
+        let next_energy = self.device.total_energy_consumption().unwrap();
+        (next_energy - prev_energy) as f64 / 1000.0
+    }
+
+    fn elapsed_mut(&mut self) -> f64 {
+        let prev_energy = self.energy;
+        let next_energy = self.device.total_energy_consumption().unwrap();
+        self.energy = next_energy;
+        (next_energy - prev_energy) as f64 / 1000.0
+    }
 }
