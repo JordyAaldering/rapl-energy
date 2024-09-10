d27fcd01df97b607f6ecb2f4f7839975576bfd11
Jordy Aaldering <aalderingjordy@gmail.com>
2024-09-10 21:20:23 +0200

    cleanup tests

--- examples/rapl.c
+++ examples/rapl.c
@@ -9,15 +9,8 @@ int main()
     rapl = start_rapl();
     sleep(1);
     print_energy(rapl);
-    sleep(1);
-    print_energy(rapl);
-    sleep(1);
-    print_energy(rapl);
 
     free_energy(rapl);
-
-    // Should segfault here
-    print_energy(rapl);
 
     return 0;
 }
