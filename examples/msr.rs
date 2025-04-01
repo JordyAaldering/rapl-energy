de31bf6fd55f456a1b4778294725b44f9bd00cc1
Jordy Aaldering <aalderingjordy@gmail.com>
2025-04-01 13:03:46 +0200

    msr example

---
+++ examples/msr.rs
@@ -0,0 +1,10 @@
+use std::thread;
+use std::time::Duration;
+
+use rapl_energy::{EnergyProbe, Msr};
+
+fn main() {
+    let msr = Msr::now().unwrap();
+    thread::sleep(Duration::from_secs(1));
+    println!("{:?}", msr.elapsed());
+}
