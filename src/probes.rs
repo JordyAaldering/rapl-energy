e40de8fe3e0e52dc838576ac078f9967e726ca67
Jordy Aaldering <aalderingjordy@gmail.com>
2025-05-25 12:19:12 +0200

    add rapl constraints

--- src/probes.rs
+++ src/probes.rs
@@ -4,8 +4,8 @@ mod msr_amd;
 #[cfg(feature = "hwmon")] mod hwmon;
 #[cfg(feature = "nvml")] mod nvml;
 
-pub use rapl::Rapl;
-pub use msr_amd::MsrAmd;
-#[cfg(feature = "http")] pub use http::Http;
-#[cfg(feature = "hwmon")] pub use hwmon::Hwmon;
-#[cfg(feature = "nvml")] pub use nvml::Nvml;
+pub use rapl::*;
+pub use msr_amd::*;
+#[cfg(feature = "http")] pub use http::*;
+#[cfg(feature = "hwmon")] pub use hwmon::*;
+#[cfg(feature = "nvml")] pub use nvml::*;
