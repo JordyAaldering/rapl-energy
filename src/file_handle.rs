7bcf80f6507d5475bdcdbf00491d253f2520b3d1
Jordy Aaldering <aalderingjordy@gmail.com>
2025-05-26 14:44:58 +0200

    use writeln instead

--- src/file_handle.rs
+++ src/file_handle.rs
@@ -27,8 +27,7 @@ impl FileHandle {
 
     pub fn write(&self, value: u64) {
         let mut file = self.open(0);
-        let buf = value.to_le_bytes();
-        file.write_all(&buf).expect("Error writing to file");
+        writeln!(file, "{}", value).unwrap();
     }
 
     pub fn from_le_bytes(&self, offset: u64) -> u64 {
